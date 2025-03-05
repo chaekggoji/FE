7b3958d7f9758b09f86c3ecf9f1a32d5fe331cd4
okang1014 <okang1014@gmail.com>
2025-03-05 14:15:36 +0900

    :lipstick: Create.jsx 데스크탑 레이아웃 조정(가운데 정렬)

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,9 +1,11 @@
+import CustomInputField from '@components/common/InputField';
+import SearchField from '@components/common/SearchField';
 import styled from 'styled-components';
 
 const MainContainer = styled.main`
+  margin: 80px 130px;
   border: grey 1px solid;
   border-radius: 8px;
-  max-width: 1100px;
   padding: 60px;
   display: flex;
   flex-direction: column;
@@ -14,12 +16,6 @@ const SearchResult = styled.div`
   display: flex;
   flex-direction: column;
   gap: 40px;
-`;
-
-const SearchField = styled.div`
-  border: grey 1px solid;
-  display: flex;
-  justify-content: space-between;
 `;
 
 const SectionTitle = styled.h2`
@@ -35,17 +31,18 @@ const Create = () => {
       <h1>스터디 생성 페이지 테스트</h1>
       <MainContainer>
         <div>page navigation</div>
-        <SearchField>
-          <input type="text" placeholder="검색어를 입력해주세요." />
-          <button type="button">search</button>
-        </SearchField>
+        <SearchField placeholder="검색어를 입력해주세요." />
+        <SectionTitle>검색 결과</SectionTitle>
         <SearchResult>
-          <SectionTitle>검색 결과</SectionTitle>
           <article>도서 카드</article>
           <article>도서 카드</article>
           <article>도서 카드</article>
           <article>도서 카드</article>
         </SearchResult>
+        <CustomInputField
+          labelText="텍스트 입력 필드"
+          placeholder="텍스트 입력 필드"
+        />
       </MainContainer>
     </>
   );
