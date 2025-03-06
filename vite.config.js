79e6e6371e6585715d87636ee764efc12751958f
ohDASEUL <comeseul312@gmail.com>
2025-03-07 05:11:02 +0900

    🔧 vite.config.js 수정

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
-import { reactRouter } from '@react-router/dev/vite';
+import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [reactRouter(), tailwindcss()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: [
       { find: '@', replacement: '/src' },
