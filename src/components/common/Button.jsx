dd11384ce5aca5cabf299bcac729b1fe8a4c429d
ohDASEUL <comeseul312@gmail.com>
2025-03-10 04:40:52 +0900

    ✏️ Button 컴포넌트 수정

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,21 +1,5 @@
 import PropTypes from 'prop-types';
 
-const buttonSizes = {
-  large: 'h-12 px-9 text-lg',
-  medium: 'h-9 px-6 text-base',
-  small: 'h-6 px-5 text-sm',
-};
-
-const buttonTypes = {
-  'CTA Abled': 'bg-primary-300 text-white',
-  'CTA Active': 'bg-primary-400 text-white',
-  'CTA Delete': 'bg-secondary-300 text-white',
-  'CTA Delete Active': 'bg-secondary-400 text-white',
-  'CTA Disabled': 'bg-gray-300 text-white cursor-not-allowed opacity-60',
-  'CTA Lined':
-    'bg-white text-gray-400 border border-gray-400 hover:bg-gray-100',
-};
-
 const Button = ({
   size = 'medium',
   type = 'CTA Abled',
@@ -23,9 +7,25 @@ const Button = ({
   onClick,
   ...props
 }) => {
+  const sizeClass = {
+    large: 'h-12 px-9 text-lg',
+    medium: 'h-9 px-6 text-base',
+    small: 'h-6 px-5 text-sm',
+  }[size];
+
+  const typeClass = {
+    'CTA Abled': 'bg-primary-300 text-white',
+    'CTA Active': 'bg-primary-400 text-white',
+    'CTA Delete': 'bg-secondary-300 text-white',
+    'CTA Delete Active': 'bg-secondary-400 text-white',
+    'CTA Disabled': 'bg-gray-300 text-white cursor-not-allowed opacity-60',
+    'CTA Lined':
+      'bg-white text-gray-400 border border-gray-400 hover:bg-gray-100',
+  }[type];
+
   return (
     <button
-      className={`inline-flex items-center justify-center rounded-lg transition duration-200 ${buttonSizes[size]} ${buttonTypes[type]}`}
+      className={`inline-flex items-center justify-center font-normal transition duration-200 rounded-lg ${sizeClass} ${typeClass}`}
       onClick={onClick}
       {...props}
     >
