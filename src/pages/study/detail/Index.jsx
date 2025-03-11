d3d065a49b8c79062fd79c2c1b5e7609894a52bd
Donghwan Oh <imodh97@gmail.com>
2025-03-11 11:43:44 +0900

    🚧 스타일 상세 홈 작업 중

--- src/pages/study/detail/Index.jsx
+++ src/pages/study/detail/Index.jsx
@@ -1,13 +1,14 @@
+import profileImg from '@assets/icons/icon_no_profile_24.svg';
+import Button from '@components/common/Button';
+
 const StudyDetailHome = () => {
   return (
     <div>
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad harum porro,
-      accusantium non sint error consequuntur voluptate ipsam dolore id, iusto
-      doloribus veritatis voluptates esse omnis, repellat expedita! Error,
-      autem. Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-      Perferendis tempore inventore soluta non. Sed aliquam dolor officia
-      dolorum, ratione eos earum officiis illum ut consequuntur doloribus qui
-      laboriosam nemo velit.
+      <div className="bg-amber-200 w-full">
+        <img className="size-16" src={profileImg} />
+        <div></div>
+        <Button>프로필 보기</Button>
+      </div>
     </div>
   );
 };
