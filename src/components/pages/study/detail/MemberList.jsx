915a847360fffbeaedfa5fec36a87b33d1643d11
Donghwan Oh <imodh97@gmail.com>
2025-03-13 14:48:55 +0900

    ♻️ 공통 컴포넌트 생성

---
+++ src/components/pages/study/detail/MemberList.jsx
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types';
+import { useNavigate } from 'react-router-dom';
+
+// Todo
+// 2. medium size
+
+const MemberList = ({ memberList }) => {
+  const navigate = useNavigate();
+  return (
+    <div className="flex cursor-pointer">
+      {memberList.map((member) => {
+        return (
+          <div
+            key={member.id}
+            className={`bg-slate-200 border-1 border-black rounded-full relative group size-12 -mr-2`}
+            title={member.nickname ? member.nickname : '탈퇴한 사용자'}
+            onClick={() => navigate(`/profile/${member.id}`)}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+MemberList.propTypes = {
+  memberList: PropTypes.array.isRequired,
+};
+
+export default MemberList;
