00b57404d04acaa3bc44a935cf950c33afe4f0e7
okang1014 <okang1014@gmail.com>
2025-03-18 12:23:58 +0900

    :lipstick: ProgressBar.jsx 텍스트 사이즈 수정

--- src/components/pages/study/create/ProgressBar.jsx
+++ src/components/pages/study/create/ProgressBar.jsx
@@ -4,8 +4,8 @@ import { NavLink } from 'react-router';
 const ProgressBar = ({
   currentStep,
   setCurrentStep,
+  isStepZeroFilled,
   isStepOneFilled,
-  isStepTwoFilled,
 }) => {
   return (
     <div className="flex justify-center gap-x-10 sm:gap-x-28 relative w-fit mx-auto">
@@ -16,13 +16,13 @@ const ProgressBar = ({
         }}
       >
         <div className="flex justify-center items-center bg-primary-300 text-white w-10 h-10 rounded-full">
-          {isStepOneFilled ? (
+          {isStepZeroFilled ? (
             <img src="/src/assets/icons/icon_check_24.svg" />
           ) : (
             '1'
           )}
         </div>
-        <p>도서 검색</p>
+        <p className="text-2xl">도서 검색</p>
       </NavLink>
       <NavLink
         className="text-center flex flex-col items-center gap-2"
@@ -33,13 +33,13 @@ const ProgressBar = ({
         <div
           className={`flex justify-center items-center ${currentStep >= 1 ? 'bg-primary-300 text-white' : 'bg-white text-black border-2 border-primary-300'}  w-10 h-10 rounded-full`}
         >
-          {isStepTwoFilled ? (
+          {isStepOneFilled ? (
             <img src="/src/assets/icons/icon_check_24.svg" />
           ) : (
             '2'
           )}
         </div>
-        <p>스터디 정보 입력</p>
+        <p className="text-2xl">스터디 정보 입력</p>
       </NavLink>
       <NavLink
         className="text-center flex flex-col items-center gap-2"
@@ -52,7 +52,7 @@ const ProgressBar = ({
         >
           3
         </div>
-        <p>미리보기</p>
+        <p className="text-2xl">미리보기</p>
       </NavLink>
       <div className="hidden absolute top-5 w-11/12 h-[2px] bg-primary-300 -z-10 sm:block"></div>
     </div>
@@ -62,8 +62,8 @@ const ProgressBar = ({
 ProgressBar.propTypes = {
   currentStep: PropTypes.number.isRequired,
   setCurrentStep: PropTypes.func.isRequired,
+  isStepZeroFilled: PropTypes.bool.isRequired,
   isStepOneFilled: PropTypes.bool.isRequired,
-  isStepTwoFilled: PropTypes.bool.isRequired,
 };
 
 export default ProgressBar;
