adc7b0e705eaf63d1c28de28050eb1b0a5a3a28b
okang1014 <okang1014@gmail.com>
2025-03-21 13:57:28 +0900

    :art: CustomInputField, CustomTextarea, SearchField 라벨 조건부 렌더링 수정

--- src/components/common/SearchField.jsx
+++ src/components/common/SearchField.jsx
@@ -4,9 +4,11 @@ import PropTypes from 'prop-types';
 const SearchField = ({ type = 'text', placeholder, labelText, labelSize }) => {
   return (
     <>
-      <label className={`mb-1 text-gray-400 text-${labelSize}`}>
-        {labelText || null}
-      </label>
+      {labelText && (
+        <label className={`mb-1 text-gray-400 text-${labelSize}`}>
+          {labelText}
+        </label>
+      )}
       <div
         className={`flex gap-x-6 justify-between w-full pl-6 pr-2 py-2 border border-gray-200 rounded-xl sm:text-xl has-focus-within:border-primary-300 has-focus-within:shadow has-focus-within:shadow-primary-300`}
       >
