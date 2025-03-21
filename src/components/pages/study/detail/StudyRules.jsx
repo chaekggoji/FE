9bf4e7f2836d83d21d9a9b7b5725b5c50f937654
okang1014 <okang1014@gmail.com>
2025-03-21 14:07:25 +0900

    :lipstick: StudyRules.jsx 조건부 스타일링 추가

--- src/components/pages/study/detail/StudyRules.jsx
+++ src/components/pages/study/detail/StudyRules.jsx
@@ -1,8 +1,14 @@
+import { useLocation } from 'react-router';
+
 const rules = ['주 1회 토론 꼭 참여', '토론 글에 댓글 꼭 달기'];
 
 const StudyRules = () => {
+  const { pathname } = useLocation();
+
   return (
-    <div className="lg:px-24 md:py-12 py-6 border-b-1 border-slate-200">
+    <div
+      className={`lg:px-24 md:py-12 py-6 ${pathname !== '/study/create' && 'border-b-1 border-slate-200'}`}
+    >
       <h2 className="text-3xl mb-4 text-center">스터디 규칙</h2>
       <ul className="text-2xl text-center">
         {rules.map((item, index) => (
