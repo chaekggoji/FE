df0a14cbf62a4a5132e6525d2af3b73ee56c9f81
Donghwan Oh <imodh97@gmail.com>
2025-03-24 14:38:24 +0900

    ♻️ useQuery 사용해 데이터 fetch

--- src/pages/study/detail/Index.jsx
+++ src/pages/study/detail/Index.jsx
@@ -4,54 +4,43 @@ import StudyIntro from '@components/pages/study/detail/StudyIntro';
 import StudyLeader from '@components/pages/study/detail/StudyLeader';
 import StudyRules from '@components/pages/study/detail/StudyRules';
 import { getStudyById } from '@queries/getStudyById';
-import { useEffect, useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
 import { useParams } from 'react-router';
 
-// 필요 데이터
-
 const StudyDetailHome = () => {
-  const [studyData, setStudyData] = useState({});
-  const [leader, setLeader] = useState();
-  const [isLoading, setIsLoading] = useState(true);
   const { studyId } = useParams();
-  useEffect(() => {
-    const fetchData = async () => {
-      const { data, error } = await getStudyById(studyId);
 
-      if (error) {
-        console.log('Study fetch error:', error);
-        return;
-      }
+  const { data, isLoading } = useQuery({
+    queryKey: ['study', studyId],
+    queryFn: () => {
+      return getStudyById(studyId);
+    },
+    select: (res) => {
+      // 응답에서 leader를 꺼내 study와 함께 반환
+      const study = res.data;
+      const leader = study.study_participants.find(
+        (participant) => participant.role === 'leader',
+      );
 
-      if (data) {
-        setStudyData(data);
-
-        const leader = data.study_participants.find(
-          (participant) => participant.role === 'leader',
-        );
-
-        setLeader(leader);
-        setIsLoading(false);
-      }
-    };
-
-    fetchData();
-  }, [studyId]);
+      return { ...study, leader };
+    },
+    staleTime: 1000 * 10, // 10초 동안 refetch 안 함
+  });
 
   return (
     <div className="relative">
       {!isLoading && (
         <div className="flex flex-col">
           <StudyLeader
-            leaderData={leader.users}
+            leaderData={data.leader.users}
             className="lg:order-0 order-1"
           />
           <div className="lg:px-24 flex md:py-12 py-6 border-b-1 border-slate-200 md:flex-row flex-col">
-            <StudyInfo studyData={studyData} />
-            <StudyBook bookData={studyData.books} />
+            <StudyInfo studyData={data} />
+            <StudyBook bookData={data.books} />
           </div>
-          <StudyIntro introData={studyData.description} />
-          <StudyRules ruleData={studyData.rule} />
+          <StudyIntro introData={data.description} />
+          <StudyRules ruleData={data.rule} />
         </div>
       )}
     </div>
