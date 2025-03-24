7ea26f7a72fc61978c1ac1ca0089f2f092c8eeea
Donghwan Oh <imodh97@gmail.com>
2025-03-24 15:01:00 +0900

    ♻️ 스터디 참여 useMutation 사용

--- src/components/pages/study/detail/StudyInfo.jsx
+++ src/components/pages/study/detail/StudyInfo.jsx
@@ -3,11 +3,13 @@ import StudyMembers from '@components/pages/study/detail/StudyMembers';
 import { useLocation, useParams } from 'react-router';
 import useMediaQuery from '@hooks/useMediaQuery';
 import PropTypes from 'prop-types';
-import supabase from '@libs/supabase';
+import { joinStudy } from '@queries/joinStudy';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 const loggedInUserId = 3;
 
 const StudyInfo = ({ studyData }) => {
+  const queryClient = useQueryClient();
   const { studyId } = useParams();
   // 반응형 버튼 제작을 위한 커스텀 훅 사용
   const md = useMediaQuery('(min-width: 768px)');
@@ -17,25 +19,25 @@ const StudyInfo = ({ studyData }) => {
     .map((participant) => participant.users.id)
     .includes(loggedInUserId);
 
-  const participateUser = async (userId) => {
-    const { error } = await supabase
-      .from('study_participants')
-      .insert([{ user_id: userId, study_id: studyId, role: 'member' }]);
-
-    if (error) {
+  const mutation = useMutation({
+    mutationFn: ({ studyId, userId }) => {
+      return joinStudy(studyId, userId);
+    },
+    onSuccess: () => {
+      console.log('inserted');
+      queryClient.invalidateQueries(['study', studyId]); // study 상세 재조회
+      window.alert('스터디에 참여하였습니다.');
+    },
+    onError: (error) => {
+      console.log(error.message);
       window.alert('스터디 참여 중 오류가 발생했습니다.');
-      return;
-    }
-    window.alert('스터디에 참여하였습니다.');
+    },
+  });
+
+  const handleParticipate = async (studyId, userId) => {
+    mutation.mutate({ studyId, userId });
   };
 
-  const handleParticipate = async (userId) => {
-    try {
-      await participateUser(userId);
-    } catch (error) {
-      console.log(error);
-    }
-  };
   return (
     <div className="flex-3/5 flex flex-col gap-4 md:border-none border-b-1 border-slate-200 md:pb-0 pb-6">
       <h2 className="lg:text-3xl text-2xl mb-4 text-center">스터디 정보</h2>
@@ -65,7 +67,7 @@ const StudyInfo = ({ studyData }) => {
             <div className="ml-auto">
               <Button
                 size={md ? 'medium' : 'small'}
-                onClick={() => handleParticipate(loggedInUserId)}
+                onClick={() => handleParticipate(studyId, loggedInUserId)}
               >
                 스터디 참여하기
               </Button>
