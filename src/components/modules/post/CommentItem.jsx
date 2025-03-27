3cb0253839212d9ee7154b14dd97ff45589a6020
Donghwan Oh <imodh97@gmail.com>
2025-03-27 16:40:00 +0900

    ✨ 게시글 상세 조회, 댓글 확인

--- src/components/modules/post/CommentItem.jsx
+++ src/components/modules/post/CommentItem.jsx
@@ -1,15 +1,21 @@
 import profileDefaultIcon from '@assets/icons/icon_profile_default_36.svg';
 import PropTypes from 'prop-types';
 import SmallDropdownBox from '@components/common/SmallDropdownBox';
+import { getRecentActivity } from '@utils/time';
 
 const CommentItem = ({ data }) => {
   return (
     <li className="p-4 ring-2 ring-slate-300 rounded-2xl font-gowunbatang md:text-[1rem] text-sm">
       <div className="flex items-center mb-2">
-        <img src={profileDefaultIcon} className="mr-2" />
-        <p className="font-bold">유저 닉네임</p>
+        <img
+          src={data.users?.img_url ? data.users.img_url : profileDefaultIcon}
+          className="mr-2 size-9 object-cover rounded-full"
+        />
+        <p className="font-bold">{data.users?.nickname}</p>
         <div className="ml-auto flex">
-          <p className="font-bold text-slate-500 pr-2">2시간 전</p>
+          <p className="font-bold text-slate-500 pr-2">
+            {getRecentActivity(data.created_at)}
+          </p>
 
           {/* 수정, 삭제 모달 */}
           <SmallDropdownBox />
@@ -24,6 +30,8 @@ CommentItem.propTypes = {
   data: PropTypes.shape({
     id: PropTypes.number.isRequired,
     content: PropTypes.string.isRequired,
+    users: PropTypes.object.isRequired,
+    created_at: PropTypes.string.isRequired,
   }),
 };
 
--- src/components/modules/post/PostDetail.jsx
+++ src/components/modules/post/PostDetail.jsx
@@ -6,49 +6,44 @@ import { useNavigate, useParams } from 'react-router';
 import CommentItem from '@components/modules/post/CommentItem';
 import { useForm } from 'react-hook-form';
 import useMediaQuery from '@hooks/useMediaQuery';
+import { useQuery } from '@tanstack/react-query';
+import { getPostById } from '@queries/post/getPostById';
+import { getCommentListByPostId } from '@queries/post/getCommentListByPostId';
 
 const commentPlaceholder = {
   notice: '게시글에 댓글을 남겨 보세요.',
   debate: '토론 주제에 대한 자신의 생각을 남겨보세요.',
 };
 
-const post = {
-  id: 1,
-  title: '피크닉 가실 분?',
-  content:
-    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus sit aut quas fuga perferendis sed facere perspiciatis excepturi, similique tempora voluptatum voluptates expedita omnis, aliquam quos eligendi saepe recusandae adipisci ex consequatur cumque totam, reprehenderit officiis qui. Enim at illum ut minus voluptates nemo nostrum eveniet fugiat repellendus officiis. Ducimus corporis magni reiciendis ipsum eligendi vel, dolorem soluta! Adipisci voluptate nam impedit dicta sed? Optio quisquam rerum molestiae amet alias excepturi commodi quas praesentium voluptatum animi! Velit ipsam ad voluptatum officiis expedita animi, in distinctio dolorem sapiente dolores non at eius, accusantium inventore? Nobis in nam voluptates cupiditate doloremque eaque.',
-  user: {
-    id: 1,
-    nickname: '봄왔다',
-  },
-};
-
-const comments = [
-  {
-    id: 1,
-    content: '피크닉 음료수 챙겨 갈게요.',
-  },
-  {
-    id: 2,
-    content: '날씨가 너무 좋네요.',
-  },
-  {
-    id: 3,
-    content:
-      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur quo nesciunt voluptate eveniet unde officia accusantium voluptatibus veritatis aspernatur adipisci in recusandae facilis, suscipit dolores modi totam ea quos obcaecati.',
-  },
-];
 const PostDetail = () => {
-  const { boardType } = useParams();
+  const { boardType, postId } = useParams();
   const navigate = useNavigate();
   const { register, handleSubmit } = useForm();
   const md = useMediaQuery('(min-width: 768px)');
 
+  const { data: postData, isLoading: postIsLoading } = useQuery({
+    queryKey: ['post', postId],
+    queryFn: () => {
+      return getPostById(postId);
+    },
+    select: (res) => res.data,
+    staleTime: 1000 * 10,
+  });
+
+  const { data: commentListData, isLoading: commentListIsLoading } = useQuery({
+    queryKey: ['comments', postId],
+    queryFn: () => {
+      return getCommentListByPostId(postId);
+    },
+    select: (res) => res.data,
+    staleTime: 1000 * 10,
+  });
+
   const handleEdit = () => {
     navigate('edit', {
       state: {
-        title: post.title,
-        content: post.content,
+        title: postData.title,
+        content: postData.content,
       },
     });
   };
@@ -65,59 +60,71 @@ const PostDetail = () => {
   };
 
   return (
-    <div className="pb-8 lg:mx-0 md:-mx-8 sm:-mx-6">
-      <BoardTitle title={post.title} />
-      <div className="flex flex-col mx-auto max-w-[1000px] lg:px-10 md:px-8 sm:px-6 box-content">
-        {/* 작성자 정보, 수정, 삭제 버튼 */}
-        <div className="h-12 flex items-center my-2">
-          <div
-            className="cursor-pointer flex items-center"
-            onClick={() => navigate(`/profile/${post.user.id}`)}
-          >
-            <img src={profileDefaultIcon} className="mr-2" />
-            <p>{post.user.nickname}</p>
-          </div>
-          <div className="flex ml-auto gap-4">
-            <Button size={md ? 'medium' : 'small'} onClick={handleEdit}>
-              수정
-            </Button>
-            <Button
-              size={md ? 'medium' : 'small'}
-              type="CTA Delete"
-              onClick={handleDelete}
-            >
-              삭제
-            </Button>
+    <>
+      {!postIsLoading && (
+        <div className="pb-8 lg:mx-0 md:-mx-8 sm:-mx-6">
+          <BoardTitle title={postData.title} />
+          <div className="flex flex-col mx-auto max-w-[1000px] lg:px-10 md:px-8 sm:px-6 box-content">
+            {/* 작성자 정보, 수정, 삭제 버튼 */}
+            <div className="h-12 flex items-center my-2">
+              <div
+                className="cursor-pointer flex items-center"
+                onClick={() => navigate(`/profile/${postData.user.id}`)}
+              >
+                <img
+                  src={
+                    postData.users?.img_url
+                      ? postData.users.img_url
+                      : profileDefaultIcon
+                  }
+                  className="mr-2 size-9 rounded-full object-cover"
+                />
+                <p>{postData.users.nickname}</p>
+              </div>
+              <div className="flex ml-auto gap-4">
+                <Button size={md ? 'medium' : 'small'} onClick={handleEdit}>
+                  수정
+                </Button>
+                <Button
+                  size={md ? 'medium' : 'small'}
+                  type="CTA Delete"
+                  onClick={handleDelete}
+                >
+                  삭제
+                </Button>
+              </div>
+            </div>
+            {/* 글 내용 */}
+            <div className=" font-gowunbatang mb-8">{postData.content}</div>
+            {/* 댓글 */}
+            <div>
+              {/* 댓글 작성 */}
+              <form
+                className="relative mb-4 font-gowunbatang"
+                onSubmit={handleSubmit(onSubmit)}
+              >
+                <input
+                  className="w-full ring-2 ring-slate-300 focus:outline-none focus:ring-primary-400 rounded-2xl px-4 h-12"
+                  type="text"
+                  placeholder={commentPlaceholder[boardType]}
+                  {...register('comment')}
+                />
+                <button className="bg-primary-300 absolute top-0 right-0 h-12 px-3 rounded-r-2xl hover:bg-primary-400 cursor-pointer ring-2 ring-primary-300 hover:ring-primary-400">
+                  <img src={arrowUpIcon} className="size-9" />
+                </button>
+              </form>
+              {/* 댓글 목록 */}
+              <ul className="flex flex-col gap-4">
+                {!commentListIsLoading &&
+                  commentListData.map((comment) => (
+                    <CommentItem key={comment.id} data={comment} />
+                  ))}
+              </ul>
+            </div>
           </div>
         </div>
-        {/* 글 내용 */}
-        <div className=" font-gowunbatang mb-8">{post.content}</div>
-        {/* 댓글 */}
-        <div>
-          {/* 댓글 작성 */}
-          <form
-            className="relative mb-4 font-gowunbatang"
-            onSubmit={handleSubmit(onSubmit)}
-          >
-            <input
-              className="w-full ring-2 ring-slate-300 focus:outline-none focus:ring-primary-400 rounded-2xl px-4 h-12"
-              type="text"
-              placeholder={commentPlaceholder[boardType]}
-              {...register('comment')}
-            />
-            <button className="bg-primary-300 absolute top-0 right-0 h-12 px-3 rounded-r-2xl hover:bg-primary-400 cursor-pointer ring-2 ring-primary-300 hover:ring-primary-400">
-              <img src={arrowUpIcon} className="size-9" />
-            </button>
-          </form>
-          {/* 댓글 목록 */}
-          <ul className="flex flex-col gap-4">
-            {comments.map((comment) => (
-              <CommentItem key={comment.id} data={comment} />
-            ))}
-          </ul>
-        </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
