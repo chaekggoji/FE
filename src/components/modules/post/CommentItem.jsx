8493bb48f80ffb2f46a329691088a2f1b00f489d
Donghwan Oh <imodh97@gmail.com>
2025-03-27 17:25:08 +0900

    ✨ 댓글 프로필 클릭시 프로필 페이지 이동

--- src/components/modules/post/CommentItem.jsx
+++ src/components/modules/post/CommentItem.jsx
@@ -2,16 +2,26 @@ import profileDefaultIcon from '@assets/icons/icon_profile_default_36.svg';
 import PropTypes from 'prop-types';
 import SmallDropdownBox from '@components/common/SmallDropdownBox';
 import { getRecentActivity } from '@utils/time';
+import { useNavigate } from 'react-router';
 
 const CommentItem = ({ data }) => {
+  const navigate = useNavigate();
+  const handleProfileClick = () => {
+    navigate(`/profile/${data.user_id}`);
+  };
   return (
     <li className="p-4 ring-2 ring-slate-300 rounded-2xl font-gowunbatang md:text-[1rem] text-sm">
       <div className="flex items-center mb-2">
-        <img
-          src={data.users?.img_url ? data.users.img_url : profileDefaultIcon}
-          className="mr-2 size-9 object-cover rounded-full"
-        />
-        <p className="font-bold">{data.users?.nickname}</p>
+        <div
+          className="flex items-center cursor-pointer"
+          onClick={handleProfileClick}
+        >
+          <img
+            src={data.users?.img_url ? data.users.img_url : profileDefaultIcon}
+            className="mr-2 size-9 object-cover rounded-full"
+          />
+          <p className="font-bold">{data.users?.nickname}</p>
+        </div>
         <div className="ml-auto flex">
           <p className="font-bold text-slate-500 pr-2">
             {getRecentActivity(data.created_at)}
@@ -32,6 +42,7 @@ CommentItem.propTypes = {
     content: PropTypes.string.isRequired,
     users: PropTypes.object.isRequired,
     created_at: PropTypes.string.isRequired,
+    user_id: PropTypes.number.isRequired,
   }),
 };
 
