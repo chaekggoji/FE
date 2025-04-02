fdd7e44e7631e346e5c93eb75ff6d5786d17c6c8
Donghwan Oh <imodh97@gmail.com>
2025-04-02 11:48:16 +0900

    ✨ 페이지네이션 기본 구현

--- src/components/common/Pagination.jsx
+++ src/components/common/Pagination.jsx
@@ -1,19 +1,26 @@
 import PropTypes from 'prop-types';
+import { useNavigate } from 'react-router';
 
-const Pagination = ({ currentPage, setCurrentPage }) => {
+const Pagination = ({ page, setPage }) => {
+  const navigate = useNavigate();
   // 이전 버튼
   const handleMovePrevious = () => {
-    if (currentPage > 1) {
-      setCurrentPage((prev) => prev - 1);
+    if (page > 1) {
+      const newPage = page - 1;
+      setPage(newPage);
+      navigate(`?page=${newPage}`); // URL의 page 파라미터를 업데이트
     }
   };
 
   // 다음 버튼
   const handleMoveNext = () => {
-    if (currentPage < 3) {
-      setCurrentPage((prev) => prev + 1);
+    if (page < 3) {
+      const newPage = page + 1;
+      setPage(newPage);
+      navigate(`?page=${newPage}`);
     }
   };
+
   return (
     <div className="flex justify-center items-center gap-2 h-[64px] *:cursor-pointer">
       <div
@@ -22,15 +29,18 @@ const Pagination = ({ currentPage, setCurrentPage }) => {
       >
         이전
       </div>
-      {[1, 2, 3].map((page, index) => (
-        <div
-          key={index}
-          className={`px-4 py-1  border-slate-500 rounded-xl ${page === currentPage ? 'bg-primary-300 text-white' : 'border-1 border-slate-500 bg-white text-slate-500'} `}
-          onClick={() => setCurrentPage(page)}
-        >
-          {page}
-        </div>
-      ))}
+      {[1, 2, 3].map((item, index) => {
+        console.log(item, page);
+        return (
+          <div
+            key={index}
+            className={`px-4 py-1  border-slate-500 rounded-xl ${item === page ? 'bg-primary-300 text-white' : 'border-1 border-slate-500 bg-white text-slate-500'} `}
+            onClick={() => setPage(page)}
+          >
+            {item}
+          </div>
+        );
+      })}
       <div
         className="px-4 py-1 border-1 border-slate-500 rounded-xl text-slate-500"
         onClick={handleMoveNext}
@@ -42,8 +52,8 @@ const Pagination = ({ currentPage, setCurrentPage }) => {
 };
 
 Pagination.propTypes = {
-  currentPage: PropTypes.number.isRequired,
-  setCurrentPage: PropTypes.func.isRequired,
+  page: PropTypes.number.isRequired,
+  setPage: PropTypes.func.isRequired,
 };
 
 export default Pagination;
--- src/components/modules/board/Board.jsx
+++ src/components/modules/board/Board.jsx
@@ -1,7 +1,7 @@
 import Button from '@components/common/Button';
 import Pagination from '@components/common/Pagination';
 import { useEffect, useState } from 'react';
-import { useNavigate, useParams } from 'react-router';
+import { useLocation, useNavigate, useParams } from 'react-router';
 import BoardTitle from '@components/modules/board/BoardTitle';
 import BoardListItem from '@components/modules/board/BoardListItem';
 import DropdownBox from '@components/common/DropdownBox';
@@ -27,7 +27,9 @@ const options = [
 
 const Board = () => {
   const { studyId, boardType } = useParams();
+  const location = useLocation();
   const navigate = useNavigate();
+  const md = useMediaQuery('(min-width: 768px)');
 
   // 게시판 정렬 옵션
   const [selectedOption, setSelectedOption] = useState({
@@ -36,21 +38,38 @@ const Board = () => {
   });
 
   // 현재 페이지
-  const [currentPage, setCurrentPage] = useState(1);
+  const getPageFromURL = () => {
+    const params = new URLSearchParams(location.search);
+    const page = parseInt(params.get('page') || '1', 10);
+    return page;
+  };
+  const [page, setPage] = useState(getPageFromURL());
 
   useEffect(() => {
-    // boardType이 바뀔 때마다 currentPage를 1로 초기화
-    setCurrentPage(1);
+    setPage(getPageFromURL());
   }, [boardType]);
 
-  const md = useMediaQuery('(min-width: 768px)');
+  const getFromAndTo = () => {
+    const ITEM_PER_PAGE = 5;
+    const from = (page - 1) * ITEM_PER_PAGE;
+    const to = from + ITEM_PER_PAGE - 1;
+
+    // page가 1일때 : 0, 4
+    // page가 2일때 : 5, 9
+
+    return { from, to };
+  };
 
   const { data, isLoading } = useQuery({
-    queryKey: ['posts', boardType],
+    queryKey: ['posts', boardType, page],
     queryFn: () => {
-      return getPostListByType(studyId, boardType);
+      const { from, to } = getFromAndTo();
+      return getPostListByType(studyId, boardType, from, to);
     },
-    select: (res) => res.data,
+    select: (res) => ({
+      posts: res.data,
+      totalCount: res.count,
+    }),
     staleTime: 1000 * 10,
   });
 
@@ -58,44 +77,50 @@ const Board = () => {
     <div className="lg:mx-0 md:-mx-8 sm:-mx-6">
       <BoardTitle title={title[boardType]} />
       <div className="flex flex-col">
-        <div className="flex items-center px-6 h-12">
-          <DropdownBox
-            selectedOption={selectedOption}
-            setSelectedOption={setSelectedOption}
-            options={options}
-            size={md ? 'medium' : 'small'}
-          />
-          <Button
-            size={md ? 'medium' : 'small'}
-            className="ml-auto"
-            onClick={() => navigate(`/study/${studyId}/${boardType}/write`)}
-          >
-            글 작성
-          </Button>
-        </div>
-
-        <div className="font-gowunbatang min-h-[calc(100vh-264px)]">
-          <div className="border-[#835F45] border-b-1 border-t-1  text-[#835F45] hidden md:block">
-            <div className="lg:px-6 px-4 h-12 flex items-center">
-              <div className="flex-3/5 text-center">제목</div>
-              <div className="flex-2/5 flex text-center">
-                <div className="flex-1">참여자</div>
-                <div className="flex-1">댓글</div>
-                <div className="flex-1">조회수</div>
-                <div className="flex-1">최근 활동</div>
-              </div>
+        {!isLoading && (
+          <>
+            <div className="flex items-center px-6 h-12">
+              <DropdownBox
+                selectedOption={selectedOption}
+                setSelectedOption={setSelectedOption}
+                options={options}
+                size={md ? 'medium' : 'small'}
+              />
+              <Button
+                size={md ? 'medium' : 'small'}
+                className="ml-auto"
+                onClick={() => navigate(`/study/${studyId}/${boardType}/write`)}
+              >
+                글 작성
+              </Button>
             </div>
-          </div>
-          <hr className="md:hidden h-px border-0 bg-slate-500" />
-          {!isLoading &&
-            data.map((item) => <BoardListItem key={item.id} postData={item} />)}
-        </div>
-        <div className="h-[64px] flex items-center justify-center">
-          <Pagination
-            currentPage={currentPage}
-            setCurrentPage={setCurrentPage}
-          />
-        </div>
+
+            <div className="font-gowunbatang min-h-[calc(100vh-264px)]">
+              <div className="border-[#835F45] border-b-1 border-t-1  text-[#835F45] hidden md:block">
+                <div className="lg:px-6 px-4 h-12 flex items-center">
+                  <div className="flex-3/5 text-center">제목</div>
+                  <div className="flex-2/5 flex text-center">
+                    <div className="flex-1">참여자</div>
+                    <div className="flex-1">댓글</div>
+                    <div className="flex-1">조회수</div>
+                    <div className="flex-1">최근 활동</div>
+                  </div>
+                </div>
+              </div>
+              <hr className="md:hidden h-px border-0 bg-slate-500" />
+              {data?.posts.map((item) => (
+                <BoardListItem key={item.id} postData={item} />
+              ))}
+            </div>
+            <div className="h-[64px] flex items-center justify-center">
+              <Pagination
+                page={page}
+                setPage={setPage}
+                totalCount={data.totalCount}
+              />
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
