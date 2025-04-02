09a30ee3327354924978cc45b2e9cb56b7f6a31b
Donghwan Oh <imodh97@gmail.com>
2025-04-02 15:40:04 +0900

    💄 페이지 버튼 비활성화 처리

--- src/components/common/Pagination.jsx
+++ src/components/common/Pagination.jsx
@@ -2,24 +2,22 @@ import PropTypes from 'prop-types';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router';
 const ITEM_PER_PAGE = 3; // 페이지 별 게시글 개수
-const VISIBLE_PAGE_NUMBERS = 2; // 한 번에 볼수 있는 페이지 버튼 개수
+const PAGES_PER_GROUP = 2; // 한 번에 볼수 있는 페이지 버튼 개수
 
 const Pagination = ({ currentPage, setCurrentPage, totalCount }) => {
   const navigate = useNavigate();
   const [pageGroups, setPageGroups] = useState([]);
   const totalPages = Math.ceil(totalCount / ITEM_PER_PAGE);
-  const currentGroupIndex = Math.floor(
-    (currentPage - 1) / VISIBLE_PAGE_NUMBERS,
-  );
+  const currentGroupIndex = Math.floor((currentPage - 1) / PAGES_PER_GROUP);
 
   useEffect(() => {
     const newPageGroups = [];
     // i : 각 그룹의 첫 원소 지정
     // j : 0부터 시작해 i에 더해지며 group에 push 됨.
     // j는 버튼 개수보다 작아야 하며, i + j는 totalPages보다 작거나 같아야 함.
-    for (let i = 1; i <= totalPages; i += VISIBLE_PAGE_NUMBERS) {
+    for (let i = 1; i <= totalPages; i += PAGES_PER_GROUP) {
       const group = [];
-      for (let j = 0; j < VISIBLE_PAGE_NUMBERS && i + j <= totalPages; j++) {
+      for (let j = 0; j < PAGES_PER_GROUP && i + j <= totalPages; j++) {
         group.push(i + j);
       }
       newPageGroups.push(group);
@@ -51,7 +49,7 @@ const Pagination = ({ currentPage, setCurrentPage, totalCount }) => {
   return (
     <div className="flex justify-center items-center gap-2 h-[64px] *:cursor-pointer">
       <div
-        className="px-4 py-1 border-1 border-slate-500 rounded-xl text-slate-500"
+        className={`px-4 py-1 border-1 border-slate-500 rounded-xl text-slate-500 ${currentPage === 1 ? 'opacity-60 pointer-events-none' : ''}`}
         onClick={handleMovePrevious}
       >
         이전
@@ -68,7 +66,7 @@ const Pagination = ({ currentPage, setCurrentPage, totalCount }) => {
         );
       })}
       <div
-        className="px-4 py-1 border-1 border-slate-500 rounded-xl text-slate-500"
+        className={`px-4 py-1 border-1 border-slate-500 rounded-xl text-slate-500 ${currentPage === totalPages ? 'opacity-60 pointer-events-none' : ''}`}
         onClick={handleMoveNext}
       >
         다음
