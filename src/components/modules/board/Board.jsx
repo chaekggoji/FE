43a94b3738d01f79265250cbca6082accca5c190
Donghwan Oh <imodh97@gmail.com>
2025-04-04 17:17:25 +0900

    ✏️ 오타 수정

--- src/components/modules/board/Board.jsx
+++ src/components/modules/board/Board.jsx
@@ -139,7 +139,7 @@ const Board = () => {
                 currentPage={currentPage}
                 onPageChange={handlePageChange}
                 currentGroup={pagination.currentGroup}
-                hasPrev={}
+                hasPrev={pagination.hasPrev}
                 hasNext={pagination.hasNext}
               />
             </div>
