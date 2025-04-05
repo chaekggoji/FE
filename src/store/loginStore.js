70c7faae7736dac09ce770f93aa315d468f94697
ohDASEUL <comeseul312@gmail.com>
2025-04-05 22:23:27 +0900

    ✨ js-cookie를 통한 쿠키 저장 로직을 삭제하고, 상태 관리만 수행하도록 수정

--- src/store/loginStore.js
+++ src/store/loginStore.js
@@ -1,24 +1,16 @@
-import Cookies from 'js-cookie';
 import { create } from 'zustand';
-import { persist, createJSONStorage } from 'zustand/middleware';
+import { persist } from 'zustand/middleware';
 
-// 스토리지를 사용할 경우
 const useLoginStore = create(
   persist(
     (set) => ({
       user: null,
-      setUser: (user) => {
-        Cookies.set('user', JSON.stringify(user), { expires: 7 }); // 쿠키 유효기간 : 7일 후 만료
-        set({ user });
-      },
-      resetUser: () => {
-        Cookies.remove('user');
-        set({ user: null });
-      },
+      setUser: (user) => set({ user }),
+      resetUser: () => set({ user: null }),
     }),
     {
-      name: 'user', // persist 미들웨어용 이름
-      getStorage: () => localStorage, // 이건 무시해도 됨 (zustand persist 필수값)
+      name: 'user', // localStorage에 저장할 key
+      getStorage: () => localStorage,
     },
   ),
 );
