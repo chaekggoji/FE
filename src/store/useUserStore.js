3d83fdbd462f5e779914434d19f9b4aa4c6e1af0
ohDASEUL <comeseul312@gmail.com>
2025-04-07 02:25:14 +0900

    ✨ userStore에 authUser와 profile 상태 구조 설계

--- src/store/useUserStore.js
+++ src/store/useUserStore.js
@@ -1,11 +1,34 @@
 // store.js
-import { create } from 'zustand';
+// import { create } from 'zustand';
 
-const useUserStore = create((set) => ({
-  loggedInUser: {
-    id: 1,
-  },
-  setUser: (user) => set({ loggedInUser: user }),
-}));
+// const useUserStore = create((set) => ({
+//   loggedInUser: {
+//     id: 1,
+//   },
+//   setUser: (user) => set({ loggedInUser: user }),
+// }));
+
+// export default useUserStore;
+
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
+
+const useUserStore = create(
+  persist(
+    (set) => ({
+      authUser: null, // Supabase 인증 유저
+      profile: null, // users 테이블 정보
+
+      setAuthUser: (user) => set({ authUser: user }),
+      setProfile: (profile) => set({ profile }),
+
+      resetUser: () => set({ authUser: null, profile: null }),
+    }),
+    {
+      name: 'userStore',
+      getStorage: () => localStorage,
+    },
+  ),
+);
 
 export default useUserStore;
