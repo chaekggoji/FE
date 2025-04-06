f67dda4c0d99d70f64db33a1703fa89c41d45352
ohDASEUL <comeseul312@gmail.com>
2025-04-07 02:26:09 +0900

    ♻️ 헤더에서 userStore 기반으로 프로필 이미지 및 로그아웃 처리

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -4,53 +4,51 @@ import alarm from '@assets/icons/icon_alarm_37.svg';
 import noAlarm from '@assets/icons/icon_no_alarm_37.svg';
 import noProfile from '@assets/icons/icon_profile_default_36.svg';
 import { useState } from 'react';
-import useLoginStore from '@store/loginStore';
 import supabase from '@libs/supabase';
+import useUserStore from '@store/useUserStore';
 
 const Header = () => {
-  const { user, resetUser } = useLoginStore(); // Zustand 상태
-  const [isAlarm, setIsAlarm] = useState(false); // 알림은 임시 상태
+  const { authUser, profile, resetUser } = useUserStore();
+  const [isAlarm, setIsAlarm] = useState(false);
   const navigate = useNavigate();
 
-  // 로그아웃
+  const profileImg = profile?.img_url;
+
   const handleLogout = async () => {
     const { error } = await supabase.auth.signOut();
     if (error) {
       alert('로그아웃 실패: ' + error.message);
       return;
     }
-    resetUser(); // Zustand 상태 초기화
+
+    resetUser();
     alert('로그아웃 되었습니다!');
-    navigate('/login'); // 로그인 페이지로 이동
+    navigate('/login');
   };
+
   return (
     <header className="flex items-center justify-between h-[74px] px-6 py-4 shadow-md text-2xl md:text-xl sm:text-lg z-10">
-      {/* 로고 섹션 */}
       <Link to="/" className="flex items-center gap-2 text-inherit">
         <img src={logo} alt="책꼬지 로고" className="w-12 md:w-10 sm:w-8" />
         <h1 className="m-0">책꼬지</h1>
       </Link>
 
-      {/* 로그인 여부에 따른 네비게이션 */}
       <nav className="flex items-center gap-5">
-        {user ? (
+        {authUser ? (
           <>
             <Link to="#" className="hidden sm:block">
               스터디 생성
             </Link>
-            {/* 알림 */}
             <img
               src={isAlarm ? alarm : noAlarm}
-              alt={isAlarm ? '알림 있음' : '알림 없음'}
+              alt="알림"
               className="w-9 md:w-8 sm:w-7"
             />
-            {/* 프로필 이미지 */}
             <img
-              src={noProfile}
-              alt="유저이미지"
-              className="w-12 md:w-10 sm:w-8"
+              src={profileImg || noProfile}
+              alt="유저 이미지"
+              className="w-12 h-12 object-cover rounded-full"
             />
-            {/* 로그아웃 버튼(테스트를 위해 임시로) */}
             <button
               onClick={handleLogout}
               className="text-base text-gray-600 border border-gray-300 rounded-lg px-3 py-1 hover:bg-gray-100"
