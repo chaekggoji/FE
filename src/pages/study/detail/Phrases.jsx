6e24c8e37e7bfbd1e9a4ca708adf75ea74f4ffd9
Donghwan Oh <imodh97@gmail.com>
2025-04-07 19:47:01 +0900

    ⚰️ 주석 제거

--- src/pages/study/detail/Phrases.jsx
+++ src/pages/study/detail/Phrases.jsx
@@ -4,12 +4,8 @@ import PhraseItem from '@components/modules/phrase/PhraseItem';
 import PhraseWrite from '@components/modules/phrase/PhraseWrite';
 import useMediaQuery from '@hooks/useMediaQuery';
 import { getPhraseList } from '@queries/phrases';
-import {
-  useInfiniteQuery,
-  useQuery,
-  useQueryClient,
-} from '@tanstack/react-query';
-import { useEffect, useRef, useState } from 'react';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { useState } from 'react';
 import { useParams } from 'react-router';
 
 // 리팩토링 목록
@@ -30,63 +26,18 @@ const Phrases = () => {
   });
   const md = useMediaQuery('(min-width: 768px)');
 
-  // 1️⃣ 무한 스크롤 구현 X
-  // const { data, isLoading } = useQuery({
-  //   queryKey: ['phrases', studyId],
-  //   queryFn: () => {
-  //     return getPhraseList(studyId);
-  //   },
-  //   select: (res) => res.data,
-  //   staleTime: 1000 * 10,
-  // });
-
-  // 2️⃣ fetch로 데이터 5개씩 불러오기 (cursor based)
-  // const cursorRef = useRef(null);
-  // const [phrases, setPhrases] = useState([]);
-  // const [isLoading, setIsLoading] = useState(false);
-
-  // useEffect(() => {
-  //   const fetchInitialPhrases = async () => {
-  //     setIsLoading(true);
-  //     const res = await getPhraseList(studyId);
-  //     setPhrases(res.data);
-  //     if (res.data.length > 0) {
-  //       const last = res.data[res.data.length - 1];
-  //       cursorRef.current = last.created_at; // 업데이트만, 리렌더 X
-  //       console.log(cursorRef.current);
-  //     }
-  //     setIsLoading(false);
-  //   };
-
-  //   fetchInitialPhrases();
-  // }, [studyId]);
-
-  // const fetchMorePhrases = async () => {
-  //   if (cursorRef.current === null) return;
-  //   setIsLoading(true);
-
-  //   const res = await getPhraseList(studyId, cursorRef.current);
-  //   if (res.data.length > 0) {
-  //     setPhrases((prev) => [...prev, ...res.data]);
-  //     const last = res.data[res.data.length - 1];
-  //     cursorRef.current = last.created_at;
-  //   }
-  //   setIsLoading(false);
-  // };
-
-  // 3️⃣ useInfiniteQuery 무한 스크롤
-  const { data, fetchNextPage, hasNextPage, isFetchingNextPage, isLoading } =
-    useInfiniteQuery({
-      queryKey: ['phrases', studyId],
-      queryFn: async ({ pageParam = null }) => {
-        return await getPhraseList(studyId, pageParam);
-      },
-      getNextPageParam: (lastPage) => {
-        if (lastPage.length === 0) return undefined;
-        return lastPage[lastPage.length - 1].created_at;
-      },
-      staleTime: 1000 * 10,
-    });
+  // 🌀 useInfiniteQuery 무한 스크롤
+  const { data, fetchNextPage, hasNextPage, isLoading } = useInfiniteQuery({
+    queryKey: ['phrases', studyId],
+    queryFn: async ({ pageParam = null }) => {
+      return await getPhraseList(studyId, pageParam);
+    },
+    getNextPageParam: (lastPage) => {
+      if (lastPage.length === 0) return undefined;
+      return lastPage[lastPage.length - 1].created_at;
+    },
+    staleTime: 1000 * 10,
+  });
 
   return (
     <div className="pb-8 lg:mx-0 md:-mx-8 sm:-mx-6">
@@ -101,11 +52,6 @@ const Phrases = () => {
         <PhraseWrite />
       </div>
       <div className="max-w-[1000px] mx-auto lg:px-10 md:px-8 px-6 flex flex-col gap-4">
-        {/* {!isLoading &&
-          phrases.map((phrase) => (
-            <PhraseItem key={phrase.id} phraseData={phrase} />
-          ))} */}
-
         {!isLoading &&
           data?.pages.map((page, i) => (
             <div key={i} className="flex flex-col gap-4">
