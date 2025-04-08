1487b31eaf69be88457bfcdf3988b6fe9b1d1f07
eunjinim <eunjee2058@gmail.com>
2025-04-08 12:24:57 +0900

    🐛 BookModal 내 PhraseItem 컴포넌트의 옵셔널 체이닝 추가로 undefined 참조 오류 해결

--- src/components/modules/phrase/PhraseItem.jsx
+++ src/components/modules/phrase/PhraseItem.jsx
@@ -80,7 +80,9 @@ const PhraseItem = ({ phraseData }) => {
             }
             className="size-10 mr-2 rounded-full object-cover"
           />
-          <p className="font-bold">{phraseData.users.nickname}</p>
+          <p className="font-bold">
+            {phraseData?.users?.nickname || '사용자 이름 없음'}
+          </p>
         </div>
         <div className="flex ml-auto">
           <img
@@ -88,7 +90,7 @@ const PhraseItem = ({ phraseData }) => {
             className="mr-2 cursor-pointer"
             onClick={handleToggleLike}
           />
-          <p>{phraseData.likes.length}</p>
+          <p>{phraseData?.likes?.length || 0}</p>
         </div>
       </div>
     </div>
