3f8112ca28de009e15e8265e3b8623e3e93be90a
Donghwan Oh <imodh97@gmail.com>
2025-04-14 21:45:33 +0900

    ✨ image resize 적용

--- src/components/common/Editor.jsx
+++ src/components/common/Editor.jsx
@@ -1,17 +1,26 @@
 import PropTypes from 'prop-types';
-import ReactQuill from 'react-quill-new'; //import1
+import ReactQuill, { Quill } from 'react-quill-new'; //import1
 import 'react-quill-new/dist/quill.snow.css'; //import2
+import { ImageActions } from '@xeger/quill-image-actions';
+
+Quill.register('modules/imageActions', ImageActions);
 
 const Editor = ({ value, onChange, height }) => {
   // 에디터에 표시할 기능
   const modules = {
-    toolbar: [
-      [{ header: [1, 2, false] }],
-      ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-      [{ list: 'ordered' }, { indent: '-1' }, { indent: '+1' }],
-      ['link', 'image'],
-      ['clean'],
-    ],
+    imageActions: {},
+    toolbar: {
+      container: [
+        [{ header: [1, 2, false] }],
+        ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+        [{ list: 'ordered' }, { indent: '-1' }, { indent: '+1' }],
+        ['link', 'image'],
+        ['clean'],
+      ],
+      ImageResize: {
+        modules: ['Resize'],
+      },
+    },
   };
 
   // 에디터에서 사용할 기능을 제한
@@ -26,6 +35,8 @@ const Editor = ({ value, onChange, height }) => {
     'indent',
     'link',
     'image',
+    'height',
+    'width',
   ];
 
   return (
