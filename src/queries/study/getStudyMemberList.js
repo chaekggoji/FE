252269702fcc8da2bb771b513097f51cd9d21755
Donghwan Oh <imodh97@gmail.com>
2025-04-18 14:46:41 +0900

    🐛 스터디 리더 정보 표시되게 수정

--- src/queries/study/getStudyMemberList.js
+++ src/queries/study/getStudyMemberList.js
@@ -3,7 +3,7 @@ import supabase from '@libs/supabase';
 export const getStudyMemberList = (studyId) => {
   return supabase
     .from('study_participants')
-    .select('id, role, users(id, nickname, img_url)')
+    .select('id, role, users(id, nickname, img_url, intro)')
     .eq('study_id', studyId)
     .order('created_at', { ascending: true }); // 리더부터 보여주기 위함
 };
